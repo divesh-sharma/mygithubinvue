85fc0d6acdb28c54f54818b87a5088cc8e4896c7
divesh.sharma_Angular <divesh.sharma@appinventiv.com>
2021-06-21 20:52:53 +0530

    first commit

---
+++ src/utilts/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAghpXLPZZPBGONL-myOMT5NMD0JAMt0gs",
+    authDomain: "mygithubapp-f9f6d.firebaseapp.com",
+    projectId: "mygithubapp-f9f6d",
+    storageBucket: "mygithubapp-f9f6d.appspot.com",
+    messagingSenderId: "936851334073",
+    appId: "1:936851334073:web:beed24ff8014946ad50c68"
+  };
+export default firebaseConfig;
